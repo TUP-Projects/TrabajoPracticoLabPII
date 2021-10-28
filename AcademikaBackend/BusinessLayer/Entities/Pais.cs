be8f964ecce6c14caf1dbb03a5a01899d932d655
ImAgInArIaS <89558763+ImAgInArIaS@users.noreply.github.com>
2021-10-28 13:33:55 -0300

    Crear pais

---
+++ AcademikaBackend/BusinessLayer/Entities/Pais.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcademikaBackend.BusinessLayer.Entities
+{
+    public class Pais
+    {
+        public int Id_Pais { get; set; }
+        public string Nom_Pais { get; set; }
+    }
+}
