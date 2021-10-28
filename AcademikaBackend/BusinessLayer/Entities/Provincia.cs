bab3c270efde806b2dbf179262789913cab92cdd
ImAgInArIaS <89558763+ImAgInArIaS@users.noreply.github.com>
2021-10-28 13:28:55 -0300

    clase provincia

---
+++ AcademikaBackend/BusinessLayer/Entities/Provincia.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcademikaBackend.BusinessLayer.Entities
+{
+    public class Provincia
+    {
+        public int id_provincia { get; set; }
+        public string nom_provincia { get; set; }
+        public int id_pais { get; set; }
+    }
+}
