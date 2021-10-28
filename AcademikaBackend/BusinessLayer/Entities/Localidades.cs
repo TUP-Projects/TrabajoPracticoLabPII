ccec0b9dd093effdb56c9aa7dca0c4252fe500c8
ImAgInArIaS <89558763+ImAgInArIaS@users.noreply.github.com>
2021-10-28 13:37:38 -0300

    Localidades
    clase

---
+++ AcademikaBackend/BusinessLayer/Entities/Localidades.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcademikaBackend.BusinessLayer.Entities
+{
+    class Localidades
+    {
+        public int Id_Localidad { get; set; }
+        public string Nom_Localidad { get; set; }
+
+        public int Id_Provincia { get; set; }
+    }
+}
