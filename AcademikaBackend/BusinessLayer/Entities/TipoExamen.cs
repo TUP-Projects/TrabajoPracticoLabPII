c0c4f5fe66438bfd0c996c45036dcc3f7a57403a
ImAgInArIaS <89558763+ImAgInArIaS@users.noreply.github.com>
2021-10-28 14:04:31 -0300

    Create TipoExamen.cs

---
+++ AcademikaBackend/BusinessLayer/Entities/TipoExamen.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcademikaBackend.BusinessLayer.Entities
+{
+    public class TipoExamen
+    {
+        public int Id_Tipo_Examen { get; set; }
+        public string Tipo_Examen { get; set; }
+    }
+}
