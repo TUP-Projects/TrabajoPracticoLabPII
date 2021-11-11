bf952e314909bcad3892124b495334c3fe61dd59
Cristian Rojas <cristianjesusrojas.chechu@gmail.com>
2021-11-11 16:42:05 -0300

    addFormEstadistica

--- AcademikaFront/Presentacion/FrmEstadisticas.Designer.cs
+++ AcademikaFront/Presentacion/FrmEstadisticas.Designer.cs
@@ -30,16 +30,15 @@ namespace AcademikaFront.Presentacion
         private void InitializeComponent()
         {
             this.label4 = new System.Windows.Forms.Label();
-            this.lblSituacionLaboral = new System.Windows.Forms.Label();
-            this.cboSituacionLaboral = new RJCodeAdvance.RJControls.RJComboBox();
-            this.cboEstadoCivil = new RJCodeAdvance.RJControls.RJComboBox();
-            this.lblSituacionHabitacional = new System.Windows.Forms.Label();
-            this.cboSituacionHabitacional = new RJCodeAdvance.RJControls.RJComboBox();
-            this.cboEdades = new RJCodeAdvance.RJControls.RJComboBox();
-            this.lblEsadoCivil = new System.Windows.Forms.Label();
             this.lblEdad = new System.Windows.Forms.Label();
-            this.cboAlumno = new RJCodeAdvance.RJControls.RJComboBox();
-            this.lblAlumno = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.dgvEstadisticas = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Condicion = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Promedio = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CantidadExamenes = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnSearch = new FontAwesome.Sharp.IconButton();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEstadisticas)).BeginInit();
             this.SuspendLayout();
             // 
             // label4
@@ -49,174 +48,107 @@ namespace AcademikaFront.Presentacion
             this.label4.ForeColor = System.Drawing.Color.Silver;
             this.label4.Location = new System.Drawing.Point(28, 24);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(151, 30);
+            this.label4.Size = new System.Drawing.Size(257, 30);
             this.label4.TabIndex = 150;
-            this.label4.Text = "ESTADISTICAS:";
-            // 
-            // lblSituacionLaboral
-            // 
-            this.lblSituacionLaboral.AutoSize = true;
-            this.lblSituacionLaboral.ForeColor = System.Drawing.Color.Gainsboro;
-            this.lblSituacionLaboral.Location = new System.Drawing.Point(592, 209);
-            this.lblSituacionLaboral.Name = "lblSituacionLaboral";
-            this.lblSituacionLaboral.Size = new System.Drawing.Size(98, 15);
-            this.lblSituacionLaboral.TabIndex = 160;
-            this.lblSituacionLaboral.Text = "Situación laboral:";
-            // 
-            // cboSituacionLaboral
-            // 
-            this.cboSituacionLaboral.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.cboSituacionLaboral.BorderColor = System.Drawing.Color.MediumSlateBlue;
-            this.cboSituacionLaboral.BorderSize = 1;
-            this.cboSituacionLaboral.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDown;
-            this.cboSituacionLaboral.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.cboSituacionLaboral.ForeColor = System.Drawing.Color.DimGray;
-            this.cboSituacionLaboral.IconColor = System.Drawing.Color.MediumSlateBlue;
-            this.cboSituacionLaboral.ListBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(228)))), ((int)(((byte)(245)))));
-            this.cboSituacionLaboral.ListTextColor = System.Drawing.Color.DimGray;
-            this.cboSituacionLaboral.Location = new System.Drawing.Point(696, 194);
-            this.cboSituacionLaboral.MinimumSize = new System.Drawing.Size(200, 30);
-            this.cboSituacionLaboral.Name = "cboSituacionLaboral";
-            this.cboSituacionLaboral.Padding = new System.Windows.Forms.Padding(1);
-            this.cboSituacionLaboral.Size = new System.Drawing.Size(200, 30);
-            this.cboSituacionLaboral.TabIndex = 159;
-            this.cboSituacionLaboral.Texts = "";
-            // 
-            // cboEstadoCivil
-            // 
-            this.cboEstadoCivil.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.cboEstadoCivil.BorderColor = System.Drawing.Color.MediumSlateBlue;
-            this.cboEstadoCivil.BorderSize = 1;
-            this.cboEstadoCivil.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDown;
-            this.cboEstadoCivil.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.cboEstadoCivil.ForeColor = System.Drawing.Color.DimGray;
-            this.cboEstadoCivil.IconColor = System.Drawing.Color.MediumSlateBlue;
-            this.cboEstadoCivil.ListBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(228)))), ((int)(((byte)(245)))));
-            this.cboEstadoCivil.ListTextColor = System.Drawing.Color.DimGray;
-            this.cboEstadoCivil.Location = new System.Drawing.Point(696, 129);
-            this.cboEstadoCivil.MinimumSize = new System.Drawing.Size(200, 30);
-            this.cboEstadoCivil.Name = "cboEstadoCivil";
-            this.cboEstadoCivil.Padding = new System.Windows.Forms.Padding(1);
-            this.cboEstadoCivil.Size = new System.Drawing.Size(200, 30);
-            this.cboEstadoCivil.TabIndex = 158;
-            this.cboEstadoCivil.Texts = "";
-            // 
-            // lblSituacionHabitacional
-            // 
-            this.lblSituacionHabitacional.AutoSize = true;
-            this.lblSituacionHabitacional.ForeColor = System.Drawing.Color.Gainsboro;
-            this.lblSituacionHabitacional.Location = new System.Drawing.Point(267, 209);
-            this.lblSituacionHabitacional.Name = "lblSituacionHabitacional";
-            this.lblSituacionHabitacional.Size = new System.Drawing.Size(89, 15);
-            this.lblSituacionHabitacional.TabIndex = 157;
-            this.lblSituacionHabitacional.Text = "Situación habit:";
-            // 
-            // cboSituacionHabitacional
-            // 
-            this.cboSituacionHabitacional.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.cboSituacionHabitacional.BorderColor = System.Drawing.Color.MediumSlateBlue;
-            this.cboSituacionHabitacional.BorderSize = 1;
-            this.cboSituacionHabitacional.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDown;
-            this.cboSituacionHabitacional.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.cboSituacionHabitacional.ForeColor = System.Drawing.Color.DimGray;
-            this.cboSituacionHabitacional.IconColor = System.Drawing.Color.MediumSlateBlue;
-            this.cboSituacionHabitacional.ListBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(228)))), ((int)(((byte)(245)))));
-            this.cboSituacionHabitacional.ListTextColor = System.Drawing.Color.DimGray;
-            this.cboSituacionHabitacional.Location = new System.Drawing.Point(362, 194);
-            this.cboSituacionHabitacional.MinimumSize = new System.Drawing.Size(200, 30);
-            this.cboSituacionHabitacional.Name = "cboSituacionHabitacional";
-            this.cboSituacionHabitacional.Padding = new System.Windows.Forms.Padding(1);
-            this.cboSituacionHabitacional.Size = new System.Drawing.Size(200, 30);
-            this.cboSituacionHabitacional.TabIndex = 156;
-            this.cboSituacionHabitacional.Texts = "";
-            // 
-            // cboEdades
-            // 
-            this.cboEdades.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.cboEdades.BorderColor = System.Drawing.Color.MediumSlateBlue;
-            this.cboEdades.BorderSize = 1;
-            this.cboEdades.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDown;
-            this.cboEdades.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.cboEdades.ForeColor = System.Drawing.Color.DimGray;
-            this.cboEdades.IconColor = System.Drawing.Color.MediumSlateBlue;
-            this.cboEdades.ListBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(228)))), ((int)(((byte)(245)))));
-            this.cboEdades.ListTextColor = System.Drawing.Color.DimGray;
-            this.cboEdades.Location = new System.Drawing.Point(362, 129);
-            this.cboEdades.MinimumSize = new System.Drawing.Size(200, 30);
-            this.cboEdades.Name = "cboEdades";
-            this.cboEdades.Padding = new System.Windows.Forms.Padding(1);
-            this.cboEdades.Size = new System.Drawing.Size(200, 30);
-            this.cboEdades.TabIndex = 155;
-            this.cboEdades.Texts = "";
-            // 
-            // lblEsadoCivil
-            // 
-            this.lblEsadoCivil.AutoSize = true;
-            this.lblEsadoCivil.ForeColor = System.Drawing.Color.Gainsboro;
-            this.lblEsadoCivil.Location = new System.Drawing.Point(621, 144);
-            this.lblEsadoCivil.Name = "lblEsadoCivil";
-            this.lblEsadoCivil.Size = new System.Drawing.Size(69, 15);
-            this.lblEsadoCivil.TabIndex = 154;
-            this.lblEsadoCivil.Text = "Estado civil:";
+            this.label4.Text = "ESTADISTICAS ALUMNOS:";
             // 
             // lblEdad
             // 
             this.lblEdad.AutoSize = true;
             this.lblEdad.ForeColor = System.Drawing.Color.Gainsboro;
-            this.lblEdad.Location = new System.Drawing.Point(320, 144);
+            this.lblEdad.Location = new System.Drawing.Point(28, 77);
             this.lblEdad.Name = "lblEdad";
-            this.lblEdad.Size = new System.Drawing.Size(36, 15);
+            this.lblEdad.Size = new System.Drawing.Size(61, 15);
             this.lblEdad.TabIndex = 153;
-            this.lblEdad.Text = "Edad:";
+            this.lblEdad.Text = "Filtrar por:";
             // 
-            // cboAlumno
+            // comboBox1
             // 
-            this.cboAlumno.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.cboAlumno.BorderColor = System.Drawing.Color.MediumSlateBlue;
-            this.cboAlumno.BorderSize = 1;
-            this.cboAlumno.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDown;
-            this.cboAlumno.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.cboAlumno.ForeColor = System.Drawing.Color.DimGray;
-            this.cboAlumno.IconColor = System.Drawing.Color.MediumSlateBlue;
-            this.cboAlumno.ListBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(228)))), ((int)(((byte)(245)))));
-            this.cboAlumno.ListTextColor = System.Drawing.Color.DimGray;
-            this.cboAlumno.Location = new System.Drawing.Point(88, 129);
-            this.cboAlumno.MinimumSize = new System.Drawing.Size(200, 30);
-            this.cboAlumno.Name = "cboAlumno";
-            this.cboAlumno.Padding = new System.Windows.Forms.Padding(1);
-            this.cboAlumno.Size = new System.Drawing.Size(200, 30);
-            this.cboAlumno.TabIndex = 162;
-            this.cboAlumno.Texts = "";
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Edad",
+            "Estado Civil",
+            "Situación Habitacional",
+            "Situación Laboral"});
+            this.comboBox1.Location = new System.Drawing.Point(95, 69);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(178, 23);
+            this.comboBox1.TabIndex = 156;
             // 
-            // lblAlumno
+            // dgvEstadisticas
             // 
-            this.lblAlumno.AutoSize = true;
-            this.lblAlumno.ForeColor = System.Drawing.Color.Gainsboro;
-            this.lblAlumno.Location = new System.Drawing.Point(28, 144);
-            this.lblAlumno.Name = "lblAlumno";
-            this.lblAlumno.Size = new System.Drawing.Size(53, 15);
-            this.lblAlumno.TabIndex = 161;
-            this.lblAlumno.Text = "Alumno:";
+            this.dgvEstadisticas.AllowUserToAddRows = false;
+            this.dgvEstadisticas.AllowUserToDeleteRows = false;
+            this.dgvEstadisticas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvEstadisticas.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Condicion,
+            this.Promedio,
+            this.CantidadExamenes});
+            this.dgvEstadisticas.Location = new System.Drawing.Point(28, 117);
+            this.dgvEstadisticas.Name = "dgvEstadisticas";
+            this.dgvEstadisticas.ReadOnly = true;
+            this.dgvEstadisticas.RowTemplate.Height = 25;
+            this.dgvEstadisticas.Size = new System.Drawing.Size(747, 360);
+            this.dgvEstadisticas.TabIndex = 157;
+            // 
+            // Column1
+            // 
+            this.Column1.DataPropertyName = "ChangeNameColumn";
+            this.Column1.HeaderText = "Column1";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // Condicion
+            // 
+            this.Condicion.DataPropertyName = "Condicion";
+            this.Condicion.HeaderText = "Condicion";
+            this.Condicion.Name = "Condicion";
+            this.Condicion.ReadOnly = true;
+            // 
+            // Promedio
+            // 
+            this.Promedio.DataPropertyName = "Promedio";
+            this.Promedio.HeaderText = "Promedio";
+            this.Promedio.Name = "Promedio";
+            this.Promedio.ReadOnly = true;
+            // 
+            // CantidadExamenes
+            // 
+            this.CantidadExamenes.DataPropertyName = "CantidadExamenes";
+            this.CantidadExamenes.HeaderText = "Cantidad de Examenes";
+            this.CantidadExamenes.Name = "CantidadExamenes";
+            this.CantidadExamenes.ReadOnly = true;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.IconChar = FontAwesome.Sharp.IconChar.Search;
+            this.btnSearch.IconColor = System.Drawing.Color.Black;
+            this.btnSearch.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnSearch.IconSize = 25;
+            this.btnSearch.Location = new System.Drawing.Point(293, 64);
+            this.btnSearch.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(30, 30);
+            this.btnSearch.TabIndex = 158;
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
             // 
             // FrmEstadisticas
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(59)))), ((int)(((byte)(76)))), ((int)(((byte)(117)))));
-            this.ClientSize = new System.Drawing.Size(1154, 711);
-            this.Controls.Add(this.cboAlumno);
-            this.Controls.Add(this.lblAlumno);
-            this.Controls.Add(this.lblSituacionLaboral);
-            this.Controls.Add(this.cboSituacionLaboral);
-            this.Controls.Add(this.cboEstadoCivil);
-            this.Controls.Add(this.lblSituacionHabitacional);
-            this.Controls.Add(this.cboSituacionHabitacional);
-            this.Controls.Add(this.cboEdades);
-            this.Controls.Add(this.lblEsadoCivil);
+            this.ClientSize = new System.Drawing.Size(806, 501);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.dgvEstadisticas);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.lblEdad);
             this.Controls.Add(this.label4);
             this.Name = "FrmEstadisticas";
             this.Text = "FrmEstadisticas";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEstadisticas)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -225,15 +157,14 @@ namespace AcademikaFront.Presentacion
         #endregion
 
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label lblSituacionLaboral;
-        private RJCodeAdvance.RJControls.RJComboBox cboSituacionLaboral;
-        private RJCodeAdvance.RJControls.RJComboBox cboEstadoCivil;
-        private System.Windows.Forms.Label lblSituacionHabitacional;
-        private RJCodeAdvance.RJControls.RJComboBox cboSituacionHabitacional;
-        private RJCodeAdvance.RJControls.RJComboBox cboEdades;
-        private System.Windows.Forms.Label lblEsadoCivil;
         private System.Windows.Forms.Label lblEdad;
-        private RJCodeAdvance.RJControls.RJComboBox cboAlumno;
-        private System.Windows.Forms.Label lblAlumno;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Condicion;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Promedio;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CantidadExamenes;
+        private System.Windows.Forms.DataGridView dgvEstadisticas;
+        private FontAwesome.Sharp.IconButton btnSearch;
     }
 }
